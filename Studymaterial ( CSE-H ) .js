e2615831845376ce9315d88535386f5f5a8e37fc
YagneshwarJajala <yagneshwarjajala2007@gmail.com>
2026-08-14 22:34:46 +0530

    Dark theme

--- Studymaterial ( CSE-H ) .js
+++ Studymaterial ( CSE-H ) .js
@@ -1,12 +1,18 @@
 /* THEME-TOGGLER */
 
 const theme=document.getElementById('theme-toggle');
+const savedtheme=localStorage.getItem('theme');
+if(savedtheme === "dark"){
+    document.body.dataset.theme="dark";
+}
 theme.addEventListener("click",function(){
     if(document.body.dataset.theme === "dark"){
         document.body.removeAttribute("data-theme");
+        localStorage.setItem("theme","light");
     }
     else{
         document.body.dataset.theme="dark";
+        localStorage.setItem("theme","dark");
     }
 });
 
